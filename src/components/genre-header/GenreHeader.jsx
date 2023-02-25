71c86181230f83a61fb46b777d2049ec06a14ddf
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-02-26 04:59:25 +0530

    feat: when no data it should show first screen

--- src/components/genre-header/GenreHeader.jsx
+++ src/components/genre-header/GenreHeader.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import popimg from '../../assets/genre-pop.png';
+import Pop from '../../assets/genre-pop.png';
+import Rock from '../../assets/genre-rock.png';
+import Bollywood from '../../assets/genre-bollywood.png';
+import Country from '../../assets/genre-country.png';
 import './GenreHeader.css';
 
 export default function GenreHeader({genre}) {
+  
   //console.log(genre);
   return (
     <div className='genre-pop'>
       <div className='genre-pop-img-text'>
-        <img className='pop-img' src={popimg} alt='popimg' />
+        <img className='pop-img' src={genre === 'Pop' ? Pop : genre === 'Rock' ? Rock : genre === 'Bollywood' ? Bollywood : Country} alt='popimg' />
         <a>{genre}</a>
       </div>
     </div>
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,18 +1,35 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {BASE_URL, API_URL} from '../../constants/apiUrl';
 import {useNavigate} from 'react-router-dom';
 import Empty from '../pages/empty-body/Empty';
 import Home from '../pages/home-page/Home';
 
 export default function Main() {
   const [clickSync, setClickSync] = useState(false);
+  const [songs, setSongs] = useState([]);
+  const fetchSongs = async () => {
+    const data = await axios(`${BASE_URL}/${API_URL}`,{
+      headers: {
+        'Authorization': 'Basic QWlzaHdhcnlhIE4='
+      }});
+    setSongs(data.data.data);
+  };
+  useEffect(() => {
+    fetchSongs();
+  }, []);
   const navigate = useNavigate();
   const clickHandler = () => {
+    if(songs.length === 0){
+      setClickSync(false);
+      return;
+    }
     setClickSync(true);
     navigate('/home');
   };
   return (
     <div >
-      {clickSync ? <Home /> : <Empty clickHandler={clickHandler} />}
+      {clickSync ? <Home/> : <Empty clickHandler={clickHandler} />}
     </div>
   );
 }
